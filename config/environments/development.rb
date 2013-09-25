be665df6d3abe9902f52ac54518f208cdb076d7f
Kei HAYASHI <kei.h.hayashi@gmail.com>
2013-09-25 14:10:15 +0900

    make assets log quiet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Recommender::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Logging
+  config.assets.logger = false
 end
