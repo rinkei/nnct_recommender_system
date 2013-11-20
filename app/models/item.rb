609be4c862a2c41957b63bfffa3ac3955f7f765d
Kei HAYASHI <kei.h.hayashi@gmail.com>
2013-11-20 23:07:20 +0900

    arrange Item.recommended_for method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,23 +2,27 @@ class Item < ActiveRecord::Base
   has_many :ratings, -> { order("id desc") }
   has_many :raters, -> { uniq }, source: :user, through: :ratings
 
-  def self.recommended_for(user, limit_num: Settings.recommender_num)
-    similar_users = user.similar_users.limit(limit_num)
+  def self.recommended_for(user, similars_num: Settings.recommender_num)
+    similars = user.similar_users.limit(similars_num)
 
-    # similar ratings collecting
-    # similar_users_ratings is a hash, assumes item_id is key and Rating array is value
-    similar_users_ratings = similar_users.map{|user| user.ratings}.flatten.group_by{|rating| rating.item_id}
+    # collecting similar users' ratings
+    # similars_ratings is a hash, assume item_id is key and Rating array is value
+    similars_ratings = similars.map{|user| user.ratings}.flatten.group_by{|rating| rating.item_id}
+    sorted_similars_ratings = similars_ratings.sort_by{|k, v| -v.size}
 
     # arranged_ratings is a hash that the Item's id is key and score average is value
-    recommendation_scores = similar_users_ratings.inject({}) do|x, kv|
-      x[kv[0]] = kv[1].inject(0){|sum, r| sum += r.score}/kv[1].size.to_f # kv is array [key, value]
-      x
+    recommendation_scores = sorted_similars_ratings.inject({}) do|hash, kv|
+      # kv is array: [key(item_id), value(rating)]
+      hash[kv[0]] = kv[1].inject(0){|sum, rating| sum += rating.score}/kv[1].size.to_f
+      hash
     end
 
     # change to array that sorted score desc
     sorted_recommendation_scores = recommendation_scores.sort{|a, b| b[1] <=> a[1]}
+    sorted_ids = sorted_recommendation_scores.map{|rating| rating[0]}
 
     # return recommend items 
-    Item.find(sorted_recommendation_scores.map{|rating| rating[0]})    
+    items = Item.find(sorted_ids) - user.rated_items
+    items = sorted_ids.collect{|id| items.detect{|item| item.id == id}}
   end
 end
--- lib/tasks/calculate_similarity.rake
+++ lib/tasks/calculate_similarity.rake
@@ -7,7 +7,7 @@ task calculate_similarity: :environment do
   @users.each do |user|
     @targets.each do |trgt|
       # 新規ユーザーで見つからなかった場合はユーザ類似度モデルを作成
-      next if user == trgt
+      next if user.id == trgt.id
       us = UserSimilarity.find_or_create_by!(user: user, target: trgt)
 
       user_ratings_absolute_value_sum = 0 # ユーザ評価値の二乗の和の平方根
